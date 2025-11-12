476d472b3f0be791f1d421ed565dad9cb41e3452
Tom Donohue <monodot@gmail.com@>
2025-11-12 09:50:08 +0000

    Initial commit of .NET 4.8 application from template

---
+++ CheeseApp-net48/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CheeseApp.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ CheeseApp-net48/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CheeseApp.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
